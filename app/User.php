a07c2a203483c3475e740e66eea7bb15d6d12230
sinkcup <sinkcup@live.it>
2020-05-11 19:14:47 +0800

    feat: swagger #26

--- app/User.php
+++ app/User.php
@@ -6,6 +6,17 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
+/**
+ * Class User
+ * @package App
+ * @OA\Schema(
+ *     title="用户",
+ *     required={"name", "email", "password"},
+ *      @OA\Property(type="string", property="name", description="昵称", example="马猴"),
+ *      @OA\Property(type="string", property="email", description="邮箱", example="ma.hou@example.com"),
+ *      @OA\Property(type="string", property="password", description="密码", example="1234asdf"),
+ * )
+ */
 class User extends Authenticatable
 {
     use Notifiable;
